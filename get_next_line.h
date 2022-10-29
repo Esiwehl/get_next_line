a700cbdc81762254b340daca859091b6f6712ea3
Eluse Wehl <ewehl@f0r5s10.codam.nl>
2022-10-29 22:02:24 +0200

    Skeleton is ug againg !

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ewehl <ewehl@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/29 21:03:06 by ewehl         #+#    #+#                 */
+/*   Updated: 2022/10/29 21:05:36 by ewehl         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
