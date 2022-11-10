749d8266c26328434733f320e5a051a349242dae
Eluse Wehl <ewehl@f0r1s10.codam.nl>
2022-11-10 16:25:20 +0100

    Jup, I def hate merge conflicts.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ewehl <ewehl@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/29 21:01:32 by ewehl         #+#    #+#                 */
-/*   Updated: 2022/11/10 16:21:29 by ewehl         ########   odam.nl         */
+/*   Updated: 2022/11/10 16:24:50 by ewehl         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,46 +110,6 @@ char	*get_next_line(int fd)
 	printf("line = %s\n", line);
 	printf("ret_line = %s\n", ret_line);
 	return (ret_line);
-}
-
-#include <fcntl.h>
-int	main(void)
-{ 
-	const int	fd1 = open("foo.txt", O_RDONLY);
-	const int	fd2 = open("foo.txt", O_RDONLY);
-	// char 		*str = "cheese";
-	char *gnl;
-
-	if (fd1 < 0){
-		printf("I failed you\n");
-		printf("I failed you\n");}
-	else{
-		printf("Opened fd:: %d\n", fd1);}
-		// printf("Opened fd:: %d\n", fd2);}
-	
-	// while (str)
-	// {
-	// 	str = get_next_line(fd1);
-	// 	printf("fd1:: %s\n", str);
-	// 	free(str);
-	// 	str = get_next_line(fd2);
-	// 	printf("fd2:: %s\n", str);
-	// 	free(str);
-	// }
-
-	// gnl = get_next_line(fd1);
-	// printf("1 GNL ret:: %s\n", gnl);
-	// // char c = 0; read(fd1, &c, 1); printf("c = %c\n", c);
-	// free(gnl);
-	// gnl = get_next_line(fd2);
-	// printf("2 GNL ret:: %s\n", gnl);
-	// free(gnl);
-	
-	if (close(fd1) < 0) 
-		printf("Closed unsuccesfully :D\n");
-	else
-		printf("Closed succesfully :D\n");
-	system("leaks -q run");
 }
 
 //	gcc -Wall -Werror -Wextra -D BUFFER_SIZE=10 *.c -o run 
